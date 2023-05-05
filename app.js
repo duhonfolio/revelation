3be32ed9979b73d36efd17e823914d14422878ea
duhonfolio <ion4iot@gmail.com>
2023-05-05 13:47:11 -0500

    test Render changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { getNotes, getNote, createNote } from './database.js'
 const app = express()      // create the express app
 app.use(express.json())   // since this app becomes a JSON application, I must include. This says any JSON body will accepted and passed to the req.body
 
- // create a get route to get the list of notes
+ // create a get route to get the list of notes 
  app.get("/notes", async (req, res) => {
     const notes = await getNotes()
     res.send(notes)
@@ -24,7 +24,7 @@ app.use(express.json())   // since this app becomes a JSON application, I must i
 // error handling code from Exp
 app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).send('Something broke!')
+    res.status(500).send('Something has broken!')
   })
 
   app.listen(8080, () => {
